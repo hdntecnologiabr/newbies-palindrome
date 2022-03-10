fcb867c48e93fe9d575bd3e760527f10251d4b29
ErikFerreira477 <erikcalcada@hotmail.com>
2022-03-10 16:00:17 -0300

    Palindromo feito

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,5 +1,16 @@
 const isPalindrome = (string) => {
-  // do something here
+  let input = string;
+  let inputNospace = input.split(' ').join('');
+
+  let inputReverse = inputNospace.split('').reverse().join('');
+
+  if (inputReverse === inputNospace) {
+    console.log("É palindromo\n\n" + inputReverse + " " + inputNospace);
+    return true;
+  } else {
+    console.log("Não é palindromo\n\n" + inputReverse + " " + inputNospace);
+    return false;
+  }
 }
 
 module.exports = {
